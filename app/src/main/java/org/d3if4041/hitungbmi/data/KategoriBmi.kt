63ae19c9637a9d96a0b708a8ddb028fdbaa85626
Muhammad_Ferdhy_Ananda <ferdhy889@gmail.com>
2021-03-17 20:51:39 +0700

    Initial Commit

---
+++ app/src/main/java/org/d3if4041/hitungbmi/data/KategoriBmi.kt
@@ -0,0 +1,3 @@
+package org.d3if4041.hitungbmi.data
+
+enum class KategoriBmi { KURUS, IDEAL, GEMUK }
---
+++ app/src/main/java/org/d3if4041/hitungbmi/ui/AboutFragment.kt
@@ -0,0 +1,6 @@
+package org.d3if4041.hitungbmi.ui
+
+import androidx.fragment.app.Fragment
+import org.d3if4041.hitungbmi.R
+
+class AboutFragment : Fragment(R.layout.fragment_about)
