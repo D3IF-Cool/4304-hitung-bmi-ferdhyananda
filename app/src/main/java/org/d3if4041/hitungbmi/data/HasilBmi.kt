fec71c37cb01cb8e739b5924c359898a50e22f6d
Muhammad_Ferdhy_Ananda <ferdhy889@gmail.com>
2021-06-05 19:51:09 +0700

    Initial Commit Task 3.1 - 3.8

---
+++ app/src/main/java/org/d3if4041/hitungbmi/data/HasilBmi.kt
@@ -0,0 +1,5 @@
+package org.d3if4041.hitungbmi.data
+
+data class HasilBmi(
+    val bmi: Float,
+    val kategori: KategoriBmi)
---
+++ app/src/main/java/org/d3if4041/hitungbmi/ui/histori/HistoriViewModel.kt
@@ -0,0 +1,19 @@
+package org.d3if4041.hitungbmi.ui.histori
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.d3if4041.hitungbmi.db.BmiDao
+
+class HistoriViewModel(private val db: BmiDao) : ViewModel() {
+
+    val data = db.getLastBmi()
+
+    fun hapusData() = viewModelScope.launch {
+        withContext(Dispatchers.IO) {
+            db.clearData()
+        }
+    }
+}
